cb0ff22b3583c8a03e6f0d6a025bb34aae88d7df
Stephen Lewis <stephen.lewis2@sky.uk>
2025-07-03 11:20:23 +0100

    add a debug route

---
+++ server/api/debug.ts
@@ -0,0 +1,8 @@
+export default defineEventHandler((event) => {
+  const config = useRuntimeConfig();
+  return {
+    publicVars: config.public,
+    githubBotTokenExists: !!config.githubBotToken,
+    gitHubToken: !!config.githubToken
+  };
+});
