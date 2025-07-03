252063f2c66edcac8c3e14f4f6c7dd84b2758490
Stephen Lewis <stephen.lewis2@sky.uk>
2025-07-03 11:28:59 +0100

    simplify debug page

--- server/api/debug.ts
+++ server/api/debug.ts
@@ -1,8 +1,6 @@
 export default defineEventHandler((event) => {
   const config = useRuntimeConfig();
   return {
-    publicVars: config.public,
-    githubBotTokenExists: !!config.githubBotToken,
-    gitHubToken: !!config.githubToken
+    publicVars: config.public
   };
 });
